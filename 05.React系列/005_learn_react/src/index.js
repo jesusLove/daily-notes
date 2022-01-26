9218b38b354c263e5b836d643c43fa3196a4db04
LQQ <jesus_lqq@163.com>
2022-01-26 10:16:50 +0800

    feat: React hooks

--- 05.React系列/005_learn_react/src/index.js
+++ 05.React系列/005_learn_react/src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom'
 // import App from './01_状态更新/01_shouldComponentUpdate'
 
 // import App from './01_状态更新/02_阻止更新React.memo'
-import App from './01_状态更新/04_组织更新React.useCallback2'
+// import App from './01_状态更新/04_组织更新React.useCallback2'
+
+// import App from './03_Hooks/01_Hooks初识'
+import App from './03_Hooks/05_useReducer替代useState'
 
 ReactDOM.render(<App />, document.getElementById('root'))
