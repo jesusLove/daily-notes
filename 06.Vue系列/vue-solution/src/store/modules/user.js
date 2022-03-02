1c6dd03862f49677f91c19d6aced4bb674053098
LQQ <jesus_lqq@163.com>
2022-03-02 16:44:37 +0800

    feat: 封装登录请求、Token 缓存

--- 06.Vue系列/vue-solution/src/store/modules/user.js
+++ 06.Vue系列/vue-solution/src/store/modules/user.js
@@ -1,16 +1,26 @@
 import { login } from '@/api/sys'
+import { getItem, setItem } from '@/utils/storage'
 import md5 from 'md5'
+import { TOKEN } from '@/constant'
 
 export default {
   namespaced: true,
-  state: () => ({}),
-  mutations: {},
+  state: () => ({
+    token: getItem(TOKEN) || ''
+  }),
+  mutations: {
+    setToken(state, token) {
+      state.token = token
+      setItem(TOKEN, token)
+    }
+  },
   actions: {
     login(ctx, { username, password }) {
       return new Promise((resolve, reject) => {
         login({ username, password: md5(password) })
           .then((data) => {
-            resolve(data)
+            this.commit('user/setToken', data.token)
+            resolve()
           })
           .catch((err) => {
             reject(err)
--- 06.Vue系列/vue-solution/src/utils/request.js
+++ 06.Vue系列/vue-solution/src/utils/request.js
@@ -1,8 +1,25 @@
 import axios from 'axios'
+import { ElMessage } from 'element-plus'
 
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
   timeout: 5000
 })
+
+service.interceptors.response.use(
+  (res) => {
+    const { success, message, data } = res.data
+    if (success) {
+      return data
+    } else {
+      ElMessage.error(message)
+      return Promise.reject(new Error(message))
+    }
+  },
+  (err) => {
+    ElMessage.error(err.message)
+    return Promise.reject(err)
+  }
+)
 
 export default service
