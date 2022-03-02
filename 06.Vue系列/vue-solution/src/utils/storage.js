d30eb110d2ceaea810f0f560dc058df5171e675f
LQQ <jesus_lqq@163.com>
2022-03-02 16:43:57 +0800

    feat: 封装 LocalStorage 方法

---
+++ 06.Vue系列/vue-solution/src/utils/storage.js
@@ -0,0 +1,35 @@
+/**
+ * 存储数据
+ */
+export const setItem = (key, value) => {
+  if (typeof value === 'object') {
+    value = JSON.stringify(value)
+  }
+  localStorage.setItem(key, value)
+}
+
+/**
+ * 获取数据
+ */
+export const getItem = (key) => {
+  const data = window.localStorage.getItem(key)
+  try {
+    return JSON.parse(data)
+  } catch (err) {
+    return data
+  }
+}
+
+/**
+ * 删除数据
+ */
+export const removeItem = (key) => {
+  window.localStorage.removeItem(key)
+}
+
+/**
+ * 删除所有数据
+ */
+export const removeAllItem = (key) => {
+  window.localStorage.clear()
+}
