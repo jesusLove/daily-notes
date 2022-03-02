f02d8ef6cbb60a936c9248737103a24c57808a07
LQQ <jesus_lqq@163.com>
2022-03-02 16:49:48 +0800

    feat: 鉴权解决方案

---
+++ 06.Vue系列/vue-solution/src/permisstion.js
@@ -0,0 +1,25 @@
+import router from './router'
+import store from './store'
+
+// 白名单
+const whiteList = ['/login']
+
+// 路由守卫
+
+router.beforeEach(async (to, from, next) => {
+  if (store.getters.token) {
+    // 存在 token 进入首页
+    if (to.path === '/login') {
+      next('/')
+    } else {
+      next()
+    }
+  } else {
+    // 没有 token，进入白名单
+    if (whiteList.indexOf(to.path) > -1) {
+      next()
+    } else {
+      next('/login')
+    }
+  }
+})
